bee3d6ed2fe511a251e1b60de32b34778dd29450
zxnm19980609 <zxnm19980609@outlook.com>
2018-07-26 10:04:39 +0800

    2018 Multi-University Training Contest 1

---
+++ HDOJ/2018 Multi-University Training Contest 1/1008.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+const int MAXN = 1e6 + 10;
+int main() {
+    freopen("in.txt", "r", stdin);
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d", &n);
+
+    }
+    return 0;
+}
