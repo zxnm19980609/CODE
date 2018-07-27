eecff083bec6e80f8249aa02fd43691b1b4707fc
zxnm19980609 <zxnm19980609@outlook.com>
2018-07-27 15:24:41 +0800

    2018 Multi-University Training Contest 2

--- HDOJ/2018 Multi-University Training Contest 2/1003.cpp
+++ HDOJ/2018 Multi-University Training Contest 2/1003.cpp
@@ -1,48 +1,98 @@
-/**
- * 欧拉回路注意**联通子图**和**孤立点**
- */
 #include <cstdio>
 #include <cstring>
+#include <vector>
+#include <stack>
 using namespace std;
 const int MAXN = 100010;
 const int MAXM = 100010;
-class {
+class Edge{
 public:
-    int x, next, sign;
-}edge[MAXM << 1];
-int n, m, cnt, odc;
-int h[MAXN], degree[MAXN], odd[MAXN];
-void addEdge(int u, int v, int sign) {
+    int x, next, id, sign;
+};
+int n, m, p, cnt, odc;
+int h[MAXN], degree[MAXN], odd[MAXN], viridx[MAXN];
+bool visn[MAXN], vism[MAXM << 2];
+stack<Edge> stk;
+vector<int> ans[MAXN];
+Edge edge[MAXM << 2], tmp[MAXN];
+void addEdge(int u, int v, int id, int sign) {
     ++cnt;
     edge[cnt].x = v;
     edge[cnt].next = h[u];
+    edge[cnt].id = id;
     edge[cnt].sign = sign;
     h[u] = cnt;
 }
-void init() {
-    cnt = 0;
-    memset(h, 0, sizeof h);
-    memset(degree, 0, sizeof degree);
-    for (int u, v, i = 0; i < m; ++i) {
-        scanf("%d%d", u, v);
-        addEdge(u, v, 1);
-        addEdge(v, u, -1);
-        ++degree[u];
-        ++degree[v];
-    }
+void dfs(int u) {
+    visn[u] = true;
+    for (int p = h[u]; p; p = edge[p].next)
+        if (!vism[edge[p].id]) {
+            vism[edge[p].id] = true;
+            dfs(edge[p].x);
+            stk.push(edge[p]);
+        }
 }
 int main() {
-    freopen("in.txt", "r", stdin);
+    // freopen("in.txt", "r", stdin);
     while (~scanf("%d%d", &n, &m)) {
-        init();
+        cnt = 0;
+        memset(h, 0, sizeof h);
+        memset(degree, 0, sizeof degree);
+        for (int u, v, i = 1; i <= m; ++i) {
+            scanf("%d%d", &u, &v);
+            addEdge(u, v, i, 1);
+            addEdge(v, u, i, -1);
+            ++degree[u];
+            ++degree[v];
+        }
         odc = 0;
         for (int i = 1; i <= n; ++i)
-            if (degree[i] & 1) odd[odc++] = i;
-        for (int i = 0; i < odc; i += 2) {
-            addEdge(odd[i], odd[i + 1], 0);
-            addEdge(odd[i + 1], odd[i], 0);
+            if (degree[i] & 1) odd[++odc] = i;
+        for (int i = 1; i <= odc; i += 2) {
+            addEdge(odd[i], odd[i + 1], m + i, 0);
+            addEdge(odd[i + 1], odd[i], m + i, 0);
         }
-
+        p = 0;
+        memset(visn, 0, sizeof visn);
+        memset(vism, 0, sizeof vism);
+        for (int i = 1; i <= n; ++i)
+            if (!visn[i]) {
+                dfs(i);
+                int c = 0;
+                while (!stk.empty()) {
+                    tmp[c++] = stk.top();
+                    stk.pop();
+                }
+                if (!c) continue;
+                int virc = 0;
+                for (int j = 0; j < c; ++j)
+                    if (!tmp[j].sign) viridx[virc++] = j;
+                if (!virc) {
+                    for (int j = 0; j < c; ++j)
+                        ans[p].push_back(tmp[j].sign * tmp[j].id);
+                    ++p;
+                }
+                else {
+                    viridx[virc] = viridx[0];
+                    for (int j = 0; j < virc; ++j) {
+                        int k = (viridx[j] + 1) % c;
+                        while (k != viridx[j + 1]) {
+                            ans[p].push_back(tmp[k].sign * tmp[k].id);
+                            k = (k + 1) % c;
+                        }
+                        if (ans[p].size()) ++p;
+                    }
+                }
+            }
+        printf("%d\n", p);
+        for (int i = 0; i < p; ++i) {
+            int sz = ans[i].size();
+            printf("%d", (int)ans[i].size());
+            for (int id : ans[i])
+                printf(" %d", id);
+            printf("\n");
+            ans[i].clear();
+        }
     }
     return 0;
 }
--- HDOJ/2018 Multi-University Training Contest 2/1006.cpp
+++ HDOJ/2018 Multi-University Training Contest 2/1006.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long LL;
 const int MOD = 998244353;
 const int MAX = 3000;
-int C[MAX + 5][MAX + 5], rC[MAX + 5][MAX + 5], fac[MAX * MAX + 5];
+int C[MAX + 5][MAX + 5], rC[MAX + 5][MAX + 5], pw[MAX * MAX + 5];
 void init() {
     C[0][0] = C[1][0] = C[1][1] = 1;
     for (int i = 2; i <= MAX; ++i) {
@@ -17,10 +17,10 @@ void init() {
         for (int j = 0; j <= i; ++j)
             rC[j][i] = C[i][j];
     int MAX2 = MAX * MAX;
-    fac[0] = 1;
+    pw[0] = 1;
     for (int i = 1; i <= MAX2; ++i) {
-        fac[i] = fac[i - 1] << 1;
-        if (fac[i] >= MOD) fac[i] -= MOD;
+        pw[i] = pw[i - 1] << 1;
+        if (pw[i] >= MOD) pw[i] -= MOD;
     }
 }
 int main() {
@@ -31,7 +31,7 @@ int main() {
         int ans = 0;
         for (int i = a; i <= n; ++i)
             for (int j = b; j <= m; ++j) {
-                t = (LL)rC[a - 1][i - 1] * rC[b - 1][j - 1] % MOD * C[n][i] % MOD * C[m][j] % MOD * fac[(n - i) * (m - j)] % MOD;
+                t = (LL)rC[a - 1][i - 1] * rC[b - 1][j - 1] % MOD * C[n][i] % MOD * C[m][j] % MOD * pw[(n - i) * (m - j)] % MOD;
                 if ((i - a + j - b) & 1)
                     ans -= t;
                 else
