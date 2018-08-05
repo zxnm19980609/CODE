c697a46b0d95820f457f45d22861f6edd171ed1d
zxnm19980609 <zxnm19980609@outlook.com>
2018-08-05 10:13:49 +0800

    2018 Multi-University Training Contest 3

--- HDOJ/2018 Multi-University Training Contest 3/1008.cpp
+++ HDOJ/2018 Multi-University Training Contest 3/1008.cpp
@@ -1,7 +1,33 @@
 #include <cstdio>
+#include <vector>
+#include <queue>
 using namespace std;
+const int MAXN = 100010;
+class Node {
+public:
+    int a, b;
+}monsters[MAXN];
+int n, father[MAXN];
+vector<int> edge[MAXN];
+priority_queue<Node> pq;
+void init() {
+    for (int i = 1; i <= n; ++i)
+        edge[i].clear();
+}
 int main() {
     freopen("in.txt", "r", stdin);
-
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d", &n);
+        init(n);
+        for (int i = 1; i <= n; ++i)
+            scanf("%d%d", &monsters[i].a, &monsters[i].b);
+        for (int u, v, i = 1; i < n; ++i) {
+            scanf("%d%d", &u, &v);
+            edge[u].push_back(v);
+            edge[v].push_back(u);
+        }
+    }
     return 0;
 }
