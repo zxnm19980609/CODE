62387240e2d3d006d13c36fecafddca99fa5711a
zxnm19980609 <zxnm19980609@outlook.com>
2018-08-18 14:36:40 +0800

    Union Find Set

--- CODECHEF/GERALD07.cpp
+++ CODECHEF/GERALD07.cpp
@@ -2,46 +2,74 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
-const int MAXQ = 2e5 + 10;
-const int MAXM = 2e5 + 10;
+const int MAX = 2e5 + 10;
 class Query {
 public:
     int l, r, blockNo, id;
-}qst[MAXQ];
-int n, m, q, BLOCK, ans[MAXQ];
-int faKP1[MAXM], faK[MAXM];
-pair<int, int> edge[MAXM];
+}qst[MAX];
+int n, m, q, BLOCK, ans[MAX], ansK, ansKP1;
+int faKP1[MAX], faK[MAX], tf[MAX];
+pair<int, int> edge[MAX];
 void initFather(int father[]) {
-    for (int i = 1; i <= m; ++i)
+    for (int i = 1; i <= n; ++i)
         father[i] = i;
 }
 int unionFind(int father[], int x) {
     if (father[x] == x) return x;
     return father[x] = unionFind(father, father[x]);
 }
-int main() {
-    freopen("in.txt", "r", stdin);
-    scanf("%d%d%d", &n, &m, &q);
-    BLOCK = sqrt(m);
-    for (int i = 1; i <= m; ++i)
-        scanf("%d%d", &edge[i].first, &edge[i].second);
-    for (int i = 1; i <= q; ++i) {
-        scanf("%d%d", &qst[i].l, &qst[i].r);
-        qst[i].blockNo = qst[i].l / BLOCK + 1;
-        qst[i].id = i;
+void unionMerge(int father[], int x, int y) {
+    int fx = unionFind(father, x);
+    int fy = unionFind(father, y);
+    if (fx != fy) {
+        if (father == faKP1)
+            --ansKP1;
+        else
+            --ansK;
+        father[fx] = fy;
     }
-    sort(qst + 1, qst + n + 1, [](const Query &a, const Query &b) {
-        return a.blockNo != b.blockNo ? a.blockNo < b.blockNo : a.r < b.r;
-    });
-    initFather(faK);
-    for (int i = 1; i <= q; ++i) {
-        if (qst[i].blockNo > qst[i - 1].blockNo) {
-            ans[0] = n;
-            for (int j = 1; j <= n; ++j)
-                faKP1[j] = j;
-            for (int j = qst[i].blockNo * BLOCK; j <= qst[i].r; ++j)
-                unionFind()
+}
+int main() {
+    // freopen("in.txt", "r", stdin);
+    int T;
+    scanf("%d", &T);
+    while (T--) {
+        scanf("%d%d%d", &n, &m, &q);
+        BLOCK = sqrt(m);
+        for (int i = 1; i <= m; ++i)
+            scanf("%d%d", &edge[i].first, &edge[i].second);
+        for (int i = 1; i <= q; ++i) {
+            scanf("%d%d", &qst[i].l, &qst[i].r);
+            qst[i].blockNo = qst[i].l / BLOCK + 1;
+            qst[i].id = i;
         }
+        sort(qst + 1, qst + q + 1, [](const Query &a, const Query &b) {
+            return a.blockNo != b.blockNo ? a.blockNo < b.blockNo : a.r < b.r;
+        });
+        initFather(faK);
+        for (int i = 1; i <= q; ++i) {
+            if (qst[i].blockNo > qst[i - 1].blockNo) {
+                ansKP1 = n;
+                initFather(faKP1);
+                for (int j = qst[i].blockNo * BLOCK; j <= qst[i].r; ++j)
+                    unionMerge(faKP1, edge[j].first, edge[j].second);
+            }
+            else {
+                for (int j = max(qst[i - 1].r + 1, qst[i].blockNo * BLOCK); j <= qst[i].r; ++j)
+                    unionMerge(faKP1, edge[j].first, edge[j].second);
+            }
+            ansK = ansKP1;
+            int lim = min(qst[i].r, qst[i].blockNo * BLOCK - 1);
+            for (int j = qst[i].l; j <= lim; ++j)
+                unionMerge(faK, unionFind(faKP1, edge[j].first), unionFind(faKP1, edge[j].second));
+            ans[qst[i].id] = ansK;
+            for (int j = qst[i].l; j <= lim; ++j) {
+                faK[faKP1[edge[j].first]] = faKP1[edge[j].first];
+                faK[faKP1[edge[j].second]] = faKP1[edge[j].second];
+            }
+        }
+        for (int i = 1; i <= q; ++i)
+            printf("%d\n", ans[i]);
     }
     return 0;
 }
