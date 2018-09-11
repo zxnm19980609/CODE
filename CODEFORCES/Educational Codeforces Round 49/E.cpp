9624e165b9ee4922379b605be678149a0419d88b
zxnm19980609 <zxnm19980609@outlook.com>
2018-09-11 16:31:40 +0800

    Educational Codeforces Round 49

--- CODEFORCES/Educational Codeforces Round 49/E.cpp
+++ CODEFORCES/Educational Codeforces Round 49/E.cpp
@@ -1,6 +1,19 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long LL;
+const LL MOD = 998244353;
+LL dp[505][505];
 int main() {
     freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    int n, k;
+    cin >> n >> k;
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= i; ++j)
+            for (int p = 1; p <= j; ++p)
+                dp[i][j] = (dp[i][j] + dp[i - p][min(i - p, j)]) % MOD;
+    ans
+    for (int )
     return 0;
 }
