c1db0a6d453873b94254d6424b1aa127ba9970fc
zxnm19980609 <zxnm19980609@outlook.com>
2018-09-11 16:32:18 +0800

    Round #505

--- CODEFORCES/Round #505/D.cpp
+++ CODEFORCES/Round #505/D.cpp
@@ -1,21 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, a[705];
-bool check(int i) {
-    for (int j = 1; j <= n; ++j)
-        if (j != i && __gcd(a[i], a[j]) != 1) return true;
-    return false;
-}
+bool g[705][705], dpL[705][705], dpR[705][705];
 int main() {
     // freopen("in.txt", "r", stdin);
     scanf("%d", &n);
     for (int i = 1; i <= n; ++i)
         scanf("%d", &a[i]);
     for (int i = 1; i <= n; ++i)
-        if (!check(i)) {
-            puts("No");
+        for (int j = 1; j <= n; ++j)
+            g[i][j] = (__gcd(a[i], a[j]) != 1);
+    for (int i = 1; i <= n; ++i)
+        dpL[i][i] = dpR[i][i] = true;
+    for (int len = 2; len <= n; ++len)
+        for (int l = 1; l <= n; ++l) {
+            int r = l + len - 1;
+            if (r > n) break;
+            for (int mid = l + 1; mid <= r; ++mid)
+                dpL[l][r] |= (g[l][mid] & dpL[mid][r] & dpR[l + 1][mid]);
+            for (int mid = l; mid < r; ++mid)
+                dpR[l][r] |= (g[r][mid] & dpL[mid][r - 1] && dpR[l][mid]);
+        }
+    for (int i = 1; i <= n; ++i)
+        if (dpL[i][n] && dpR[1][i]) {
+            puts("Yes");
             return 0;
         }
-    puts("Yes");
+    puts("No");
     return 0;
 }
