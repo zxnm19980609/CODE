6dd0969e06becead49056c7adae26452e16803b9
zxnm19980609 <zxnm19980609@outlook.com>
2018-10-09 14:52:03 +0800

    Codeforces Round #513

--- CODEFORCES/Round #513/E.cpp
+++ CODEFORCES/Round #513/E.cpp
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 using LL = long long;
-const int MAXN = 1e5 + 5;
+const int MAXN = 2e5 + 5;
 vector<int> edge[MAXN];
 int n;
-LL ans;
-int dfs(int u) {
-    int sum = 0;
+LL ans, sz[MAXN], cnt[2];
+void dfs(int u, int fa, int k) {
+    sz[u] = 1;
+    ++cnt[k];
     for (int v : edge[u]) {
-        int t = dfs(v);
-        ans += (LL)t * (n - t);
-        ans -= (LL)(t - 1LL) * (n - t);
-        ans -= (LL)sum * t;
-        sum += t;
+        if (v == fa) continue;
+        dfs(v, u, k ^ 1);
+        sz[u] += sz[v];
     }
-    return sum + 1;
 }
 int main() {
     scanf("%d", &n);
     for (int u, v, i = 1; i < n; ++i) {
         scanf("%d%d", &u, &v);
         edge[u].push_back(v);
+        edge[v].push_back(u);
     }
-    dfs(1);
+    dfs(1, 0, 0);
+    for (int i = 1; i <= n; ++i)
+        ans += sz[i] * (n - sz[i]);
+     ans += cnt[0] * cnt[1];
+    ans >>= 1;
     printf("%lld\n", ans);
     return 0;
 }
