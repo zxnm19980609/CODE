2cabfca1c3b8dcfb59eb695b9c766a9fc8787c40
zxnm19980609 <zxnm19980609@outlook.com>
2018-10-09 14:52:19 +0800

    树链剖分

--- HDOJ/3966.cpp
+++ HDOJ/3966.cpp
@@ -2,75 +2,105 @@
 #define lson p << 1
 #define rson p << 1 | 1
 using namespace std;
-namespace FastIO {
-    inline char nc() {
-        static char buf[100000], *p1 = buf, *p2 = buf;
-        return p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 100000, stdin), p1 == p2) ? '@' : *p1++;
-    }
-    int over;
-    inline int nextInt() {
-        char ch = nc();
-        int s = 0;
-        while(!(ch >= '0' && ch <= '9')) {
-            ch = nc();
-            if(ch == '@') { over = 1; break; }
-        }
-        while(ch >= '0' && ch <= '9') {
-            s = s * 10 + ch - 48, ch = nc();
-            if(ch == '@') { over = 1; break; }
-        }
-        return s;
-    }
-}
+using LL = long long;
 const int MAXN = 5e4 + 5;
-const int INF = 0x3F3F3F3F;
 class STNode {
 public:
-    int left, right, tag;
-    int val;
+    int left, right;
+    LL tag, sum;
+    int len() {
+        return right - left + 1;
+    }
 }tree[MAXN << 2];
+class Edge {
+public:
+    int to, next;
+}edge[MAXN << 1];
+int cnt, h[MAXN];
 int ct, siz[MAXN], top[MAXN], son[MAXN], dep[MAXN], faz[MAXN], tid[MAXN], rnk[MAXN];
-int n, m, k, ans[MAXN];
+int n, m, q, a[MAXN];
+bool over = false;
 tuple<int, int, int> opt[MAXN];
+inline int nextInt() {
+    int x = 0;
+    char ch = getchar();
+    if (ch == EOF) {
+        over = true;
+        return x;
+    }
+    bool nagtive = false;
+    while(ch < '0' || ch > '9') {
+        if (ch == '-') nagtive = true;
+        ch = getchar();
+        if (ch == EOF) {
+            over = true;
+            break;
+        }
+    }
+    while('0' <= ch && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+        if (ch == EOF) {
+            over = true;
+            break;
+        }
+    }
+    return nagtive ? -x : x;
+}
 void init() {
-    ct = 0;
+    cnt = ct = 0;
+    memset(h, 0, sizeof h);
     memset(son, -1, sizeof son);
 }
 void build(int p, int l, int r) {
     tree[p].left = l;
     tree[p].right = r;
-    tree[p].tag = tree[p].val = 0;
-    if (l == r) return;
+    tree[p].tag  = 0;
+    if (l == r) {
+        tree[p].sum = a[rnk[l]];
+        return;
+    }
     int mid = (l + r) >> 1;
     build(lson, l, mid);
     build(rson, mid + 1, r);
+    tree[p].sum = tree[lson].sum + tree[rson].sum;
 }
 void pushdown(int p) {
     if (tree[p].tag) {
-        tree[lson].val += tree[p].tag;
-        tree[rson].val += tree[p].tag;
+        tree[lson].sum += tree[p].tag * tree[lson].len();
+        tree[rson].sum += tree[p].tag * tree[rson].len();
         tree[lson].tag += tree[p].tag;
         tree[rson].tag += tree[p].tag;
         tree[p].tag = 0;
     }
 }
-void update(int p, int l, int r) {
+LL query(int p, int x) {
+    if (tree[p].left ==  tree[p].right)
+        return tree[p].sum;
+    pushdown(p);
+    int mid = (tree[p].left + tree[p].right) >> 1;
+    if (x <= mid)
+        return query(lson, x);
+    else if (x > mid)
+        return query(rson, x);
+}
+void update(int p, int l, int r, LL c) {
     if (tree[p].left == l && tree[p].right == r) {
-        ++tree[p].val;
-        ++tree[p].tag;
+        tree[p].sum += c * tree[p].len();
+        tree[p].tag += c;
         return;
     }
     pushdown(p);
     int mid = (tree[p].left + tree[p].right) >> 1;
     if (r <= mid)
-        update(lson, l, r);
+        update(lson, l, r, c);
     else if (l > mid)
-        update(rson, l, r);
+        update(rson, l, r, c);
     else {
-        update(lson, l, mid);
-        update(rson, mid + 1, r);
+        update(lson, l, mid, c);
+        update(rson, mid + 1, r, c);
     }
-    tree[p].val = max(tree[lson].val, tree[rson].val);
+    tree[p].sum = tree[lson].sum + tree[rson].sum;
 }
 void addEdge(int u, int v) {
     ++cnt;
@@ -112,62 +142,61 @@ void updatePath(int u, int v, int c) {
     int tu = top[u], tv = top[v];
     while (tu != tv) {
         if (dep[tu] > dep[tv]) {
-            update(1, tid[tu], tid[u]);
+            update(1, tid[tu], tid[u], c);
             u = faz[tu];
             tu = top[u];
         }
         else {
-            update(1, tid[tv], tid[v]);
+            update(1, tid[tv], tid[v], c);
             v = faz[tv];
             tv = top[v];
         }
     }
     if (tid[u] < tid[v])
-        update(1, tid[u], tid[v]);
+        update(1, tid[u], tid[v], c);
     else
-        update(1, tid[v], tid[u]);
-}
-void find(int p, int c) {
-    if (tree[p].val < k) return;
-    if (tree[p].left == tree[p].right) {
-        ans[rnk[tree[p].left]] = c;
-        tree[p].val = -INF;
-        return;
-    }
-    pushdown(p);
-    int mid = (tree[p].left + tree[p].right) >> 1;
-    find(lson, c);
-    find(rson, c);
-    tree[p].val = max(tree[lson].val, tree[rson].val);
-}
-void solve(int u, int v, int c) {
-    updatePath(u, v, c);
-    find(1, c);
+        update(1, tid[v], tid[u], c);
 }
 int main() {
-    while (!FastIO::over) {
-        n = FastIO::nextInt();
-        m = FastIO::nextInt();
-        p = FastIO::nextInt();
+    while (!over) {
+        init();
+        n = nextInt();
+        m = nextInt();
+        q = nextInt();
+        for (int i = 1; i <= n; ++i)
+            a[i] = nextInt();
+        for (int u, v, i = 1; i <= m; ++i) {
+            u = nextInt();
+            v = nextInt();
+            addEdge(u, v);
+        }
+        dfs1(1, 0, 1);
+        dfs2(1, 1);
+        build(1, 1, ct);
+        while (q--) {
+            int u, v, c;
+            char ch = getchar();
+            while (ch < 'A' || ch > 'Z') ch = getchar();
+            switch (ch) {
+            case 'I':
+                u = nextInt();
+                v = nextInt();
+                c = nextInt();
+                updatePath(u, v, c);
+                break;
+            case 'D':
+                u = nextInt();
+                v = nextInt();
+                c = nextInt();
+                c = -c;
+                updatePath(u, v, c);
+                break;
+            case 'Q':
+                u = nextInt();
+                printf("%lld\n", query(1, tid[u]));
+                break;
+            }
+        }
     }
-    init();
-    scanf("%d%d%d", &n, &m, &k);
-    for (int u, v, i = 1; i < n; ++i) {
-        scanf("%d%d", &u, &v);
-        addEdge(u, v);
-    }
-    dfs1(1, 0, 1);
-    dfs2(1, 1);
-    build(1, 1, ct);
-    for (int u, v, c, i = 1; i <= m; ++i) {
-        scanf("%d%d%d", &u, &v, &c);
-        opt[i] = make_tuple(u, v, c);
-    }
-    for (int u, v, c, i = m; i; --i) {
-        tie(u, v, c) = opt[i];
-        solve(u, v, c);
-    }
-    for (int i = 1; i <= n; ++i)
-        printf(i == n ? "%d\n" : "%d ", ans[i]);
     return 0;
 }
